7172d0b8d749e426452204fb5bd838c33c376697
GuanRunwei <864037205@qq.com>
2022-04-17 14:55:43 +0800

    Signed-off-by: GuanRunwei <864037205@qq.com>

---
+++ CAT/nets/attention.py
@@ -0,0 +1,322 @@
+import torch
+import torch.nn as nn
+import math
+from functools import partial
+import numpy as np
+from einops import rearrange
+
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+# -------------------------------------------------#
+#   Basic Convolution Block
+#   Conv2d + BatchNorm2d + LeakyReLU
+# -------------------------------------------------#
+class BasicConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, kernel_size // 2, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.activation = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activation(x)
+        return x
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_()
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+
+class PatchEmbed(nn.Module):
+    def __init__(self, input_shape, in_chans, patch_size=1,  num_features=128, norm_layer=None,
+                 flatten=True):
+        super().__init__()
+        self.num_patches = (input_shape[0] // patch_size) * (input_shape[1] // patch_size)
+        self.flatten = flatten
+
+        self.proj = nn.Conv2d(in_chans, num_features, kernel_size=1, stride=1)
+        self.norm = norm_layer(num_features) if norm_layer else nn.Identity()
+
+    def forward(self, x):
+        # B, 64, 32, 32 -> B, 64, 16, 16
+        # print("first x shape", x.shape)
+        x = self.proj(x)
+
+        # B, 64, 16, 16 -> B, 64, 256 -> B, 256, 64
+        if self.flatten:
+            x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
+        x = self.norm(x)
+        # print("x shape to vit", x.shape)
+        return x
+
+
+class DropPath(nn.Module):
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+
+
+class se_block(nn.Module):
+    def __init__(self, channel, ratio=16):
+        super(se_block, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // ratio, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // ratio, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=8):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        # 利用1x1卷积代替全连接
+        self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+
+class cbam_block(nn.Module):
+    def __init__(self, channel, ratio=8, kernel_size=7):
+        super(cbam_block, self).__init__()
+        self.channelattention = ChannelAttention(channel, ratio=ratio)
+        self.spatialattention = SpatialAttention(kernel_size=kernel_size)
+
+    def forward(self, x):
+        x = x * self.channelattention(x)
+        x = x * self.spatialattention(x)
+        return x
+
+
+class eca_block(nn.Module):
+    def __init__(self, channel, b=1, gamma=2):
+        super(eca_block, self).__init__()
+        kernel_size = int(abs((math.log(channel, 2) + b) / gamma))
+        kernel_size = kernel_size if kernel_size % 2 else kernel_size + 1
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+        return x * y.expand_as(x)
+
+
+# --------------------------------------------------------------------------------------------------------------------#
+#   Global Attention
+#   divide the feature into q, k, v
+# --------------------------------------------------------------------------------------------------------------------#
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=4, qkv_bias=False, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        self.scale = (dim // num_heads) ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class Mlp(nn.Module):
+    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        drop_probs = (drop, drop)
+
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+
+
+class ViTBlock(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+        self.norm2 = norm_layer(dim)
+        self.mlp = Mlp(in_features=dim, hidden_features=int(dim * mlp_ratio), act_layer=act_layer, drop=drop)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+
+
+class GlobalAttention(nn.Module):
+    def __init__(self, channels, input_shape, patch_size=1, depth=2, num_heads=4, mlp_ratio=4.,
+                 qkv_bias=True, drop_rate=0.1, attn_drop_rate=0.1, drop_path_rate=0.05,
+                 norm_layer=partial(nn.LayerNorm, eps=1e-6), act_layer=GELU):
+        super().__init__()
+
+        self.patch_embed = PatchEmbed(input_shape=input_shape, patch_size=patch_size, in_chans=channels,
+                                      num_features=channels)
+        num_patches = (input_shape[0] // patch_size) * (input_shape[1] // patch_size)
+        self.feature_shape = [int(input_shape[0] // patch_size), int(input_shape[1] // patch_size)]
+
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, channels))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]
+        self.blocks = nn.Sequential(
+            *[
+                ViTBlock(
+                    dim=channels,
+                    num_heads=num_heads,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=dpr[i],
+                    norm_layer=norm_layer,
+                    act_layer=act_layer
+                ) for i in range(depth)
+            ]
+        )
+
+        self.norm = norm_layer(channels)
+
+    def forward_features(self, x):
+        x = self.patch_embed(x)
+
+        # img_token_pe = x
+
+        # -> batch, channel, height, width
+        img_token_pe = self.pos_embed.view(1, *self.feature_shape, -1).permute(0, 3, 1, 2)
+
+        # batch, height, width, channel -> B, N, C
+        img_token_pe = img_token_pe.permute(0, 2, 3, 1).flatten(1, 2)
+
+        x = self.pos_drop(x + img_token_pe)
+        x = self.blocks(x)
+        x = self.norm(x)
+        x = rearrange(x, 'b (w h) c -> b c w h', w=self.feature_shape[0])
+
+
+        return x
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        return x
+
+
+class ma_block(nn.Module):
+    def __init__(self, channels, input_shape):
+        super().__init__()
+
+        self.conv_attention = cbam_block(channel=channels, kernel_size=3, ratio=8)
+        self.vit_attention = GlobalAttention(channels=channels, input_shape=input_shape, patch_size=1)
+
+        self.norm = nn.BatchNorm2d(channels)
+        self.activation = nn.LeakyReLU()
+
+    def forward(self, x):
+        vit_x = x
+
+        x = self.conv_attention(x)
+        vit_x = self.vit_attention(vit_x)
+        # print("conv_attention shape:", x.shape)
+        # print("vit_attention shape:", vit_x.shape)
+
+        x = x + vit_x
+
+        x = self.norm(x)
+        x = self.activation(x)
+
+        return x
+
+
+
+
+
---
+++ CAT/summary.py
@@ -0,0 +1,14 @@
+#--------------------------------------------#
+#   该部分代码用于看网络结构
+#--------------------------------------------#
+import torch
+from torchsummary import summary
+
+from nets.yolo import YoloBody
+
+if __name__ == "__main__":
+    # 需要使用device来指定网络在GPU还是CPU运行
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    m = YoloBody([[3, 4, 5], [1, 2, 3]], 80, phi=4).to(device)
+    summary(m, input_size=(3, 640, 640))
+    print(m)
