63f256b9e14faca13f9f2d51ed659ba34787be17
Runwei Guan <31555628+GuanRunwei@users.noreply.github.com>
2022-04-26 12:36:03 +0800

    Delete MA_Summary.py

--- CAT/MA_Summary.py
+++
@@ -1,9 +0,0 @@
-import torchsummary
-import torch
-from nets.attention import ma_block
-
-
-if __name__ == '__main__':
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    ma_net = ma_block(channels=128, input_shape=[20, 20]).to(device)
-    print(torchsummary.summary(ma_net, input_size=(128, 20, 20)))
